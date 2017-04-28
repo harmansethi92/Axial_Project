385670f13f810717c4bfc20a8ee4c2ad22795ce6
harmansethi92 <harmansethi92@gmail.com>
2017-04-28 19:13:23 -0400

    Update Terraform_AWS.tf

--- Terraform_AWS.tf
+++ Terraform_AWS.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "AKIAI4V3EJIAPZO7QGBA"
+  access_key = "AKIAI4V3EJIAPZO7QGBA"                         # I have deleted these keys, you can enter your own
   secret_key = "wmTQGsRvdQMFEUZpLnJCatnJGMdvub3jXWxYqaqV"
   region     = "us-west-2"
 }
 
 resource "aws_instance" "Terraflow_AWS" {
-  ami           = "ami-2ffa634f"
+  ami           = "ami-2ffa634f"                              # This is an Image which I created
   instance_type = "t2.micro"
   count = "1"
 }
